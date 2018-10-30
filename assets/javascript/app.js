93ecb81714ec31d59fe7aa2bbeb950ee8d1f65ff
singmokjai <lijason001@gmail.com>
2018-10-30 15:36:24 -0400

    show and hide game working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,9 @@
 
 window.onload = function() {
 
-    $("#Trivia").hide();
+    $("#questions").hide();
+    $("#true").hide();
+    $("#false").hide();
 
 }
 
@@ -23,38 +25,68 @@ var seconds = 3;
 
     }
 
-    function decrement() {
-
-        seconds--;
-        $("#timer").html("<h2>" +"Time Remaining: " + seconds + "</h2>");
-
-        if (seconds === 0) {
-        
-            stop();
-            alert("Time's Up!");
-    
-        }
-
-
-    }
-
-    // Stop function
-
     function stop() {
 
         clearInterval(timer);
     
     }
 
-    // Show trivia function
+    function decrement() {
 
-    function show() {
-
-        $("#Trivia").show();
+        seconds--;
+        $("#timer").html("<h2>" +"Time Remaining: " + seconds + "</h2>");
+        
+        if (seconds === 0) {
+        
+            stop();
+            alert("Time's up!")
+            
+        }
         
     }
 
+// Show trivia function
+
+    function show() {
+
+        $("#questions").show();
+        $("#true").show();
+        $("#false").show();
         
+    }
+
+// Array of questions for trivia
+
+var questions = [
+
+    {q: "1) Michael Jordan is the best NBA player ever!", a: "True"},
+    {q: "1) Donald Trump is a great president.", a: "False"},
+];
+
+// Variables for right and wrong answers
+
+var correct = 0;
+var incorrect = 0;
+
+// Variable for holding index of questions
+
+var qIndex = 0;
+
+// Functions
+
+function nextQuestion() {
+
+    if (qIndex <= (questions.length -1)) {
+
+        document.querySelector("#questions").innerHTML = questions[qIndex].q;
+    }
+}
+
+// call function to start trivia 
+
+nextQuestion();
+
+// Function for 
 
 
 
